10975b8287cc74548241f065013763a71dde1456
Jay Chiang <jaychiang0809@berkeley.edu>
2023-02-01 04:56:04 -0800

    good

--- app.py
+++ app.py
@@ -699,5 +699,5 @@ def test_route():
 
 
 if __name__ == "__main__":
-    # port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port)
