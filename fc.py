dd6a908691de3b3a9728ff212fbd0e49580ed71c
Jay Chiang <jaychiang0809@berkeley.edu>
2023-02-09 03:12:31 -0800

    enable face recognition temp

--- fc.py
+++ fc.py
@@ -1,11 +1,11 @@
-# import face_recognition
+import face_recognition
 
-# def compare_face_data(filename1, filename2):
-#     img_1 = face_recognition.load_image_file(filename1)
-#     img_1_encoding = face_recognition.face_encodings(img_1)[0]
+def compare_face_data(filename1, filename2):
+    img_1 = face_recognition.load_image_file(filename1)
+    img_1_encoding = face_recognition.face_encodings(img_1)[0]
 
-#     img_2 = face_recognition.load_image_file(filename2)
-#     img_2_encoding = face_recognition.face_encodings(img_2)[0]
+    img_2 = face_recognition.load_image_file(filename2)
+    img_2_encoding = face_recognition.face_encodings(img_2)[0]
 
-#     results = face_recognition.compare_faces([img_1_encoding], img_2_encoding)
-#     return results[0]
+    results = face_recognition.compare_faces([img_1_encoding], img_2_encoding)
+    return results[0]
